2006763ccb794b99b79f466e52c84cd2e5cf5416
ahemmed <mohammedahemad02@gmail.com>
2023-03-08 08:04:56 +0000

    solution

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const app = express();
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "userData.db");
+let db = null;
+
+const intializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3001, () => {
+      console.log("Server is running at http://localhost:3001/");
+    });
+  } catch (e) {
+    console.log(`DB Error ${e.message}`);
+    process.exit(1);
+  }
+};
+
+intializeDbAndServer();
+
+//LOG IN USER
+app.post("/register", async (request, response) => {
+  const { username, name, password, gender, location } = request.body;
+  const userDetails = `
+    SELECT 
+        *
+    FROM 
+        user
+    WHERE 
+        username = ${username}; 
+  `;
+  const userResponse = await db.get(userDetails);
+
+  if (userResponse === undefined) {
+    const creatNewUser = `
+        INSERT INTO 
+            user(username, name, password, gender, location)
+        VALUES(
+            ${username},
+            ${name},
+            ${password},
+            ${gender},
+            ${location}
+        )
+      `;
+    await db.run(creatNewUser);
+    response.send("User created successfully");
+  } else {
+    response.status(400);
+    response.send("User already exist");
+  }
+});
