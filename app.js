04943dfe933a559e1f0999ee1941b4546d009adc
ahemmed <mohammedahemad02@gmail.com>
2023-03-09 06:50:22 +0000

    solution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const sqlite3 = require("sqlite3");
 const path = require("path");
 const app = express();
 app.use(express.json());
+const bcrypt = require("bcrypt");
 
 const dbPath = path.join(__dirname, "userData.db");
 let db = null;
@@ -28,13 +29,14 @@ intializeDbAndServer();
 //LOG IN USER
 app.post("/register", async (request, response) => {
   const { username, name, password, gender, location } = request.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
   const userDetails = `
     SELECT 
         *
     FROM 
         user
     WHERE 
-        username = ${username}; 
+        username = '${username}'; 
   `;
   const userResponse = await db.get(userDetails);
 
@@ -43,17 +45,17 @@ app.post("/register", async (request, response) => {
         INSERT INTO 
             user(username, name, password, gender, location)
         VALUES(
-            ${username},
-            ${name},
-            ${password},
-            ${gender},
-            ${location}
-        )
+            '${username}',
+            '${name}',
+            '${password}',
+            '${gender}',
+            '${location}'
+        );
       `;
     await db.run(creatNewUser);
     response.send("User created successfully");
   } else {
-    response.status(400);
+    response.status(401);
     response.send("User already exist");
   }
 });
